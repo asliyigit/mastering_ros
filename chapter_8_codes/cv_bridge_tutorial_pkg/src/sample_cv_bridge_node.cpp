3edb2362e4d5018c3cc18cb9c27a23b5048de5c8
asliyigit <180202001@kocaeli.edu.tr>
2021-04-14 15:16:43 +0300

    notlar

--- chapter_8_codes/cv_bridge_tutorial_pkg/src/sample_cv_bridge_node.cpp
+++ chapter_8_codes/cv_bridge_tutorial_pkg/src/sample_cv_bridge_node.cpp
@@ -11,36 +11,47 @@ static const std::string OPENCV_WINDOW_1 = "Edge Detection";
 class Edge_Detector
 {
   ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  image_transport::Publisher image_pub_;
+  image_transport::ImageTransport it_; // ROS görüntü mesajlarını yayınlamak ve abone olmak için kullanılan bir ImageTransport örneği oluşturur.
+  image_transport::Subscriber image_sub_; // ImageTransport sınıfının bir örneğini oluşturulur
+  image_transport::Publisher image_pub_;// ImageTransport sınıfının bir örneğini oluşturulur
   
 public:
   Edge_Detector()
     : it_(nh_)
   {
     // Subscribe to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, 
+    //image_transport nesnesini kullanarak görüntülere abone olmak ve yayınlamak için Abone(subscribe) ve Yayıncı(advertise) nesnelerini oluşturulur
+    image_sub_ = it_.subscribe("/usb_cam/image_raw", 1,  
       &Edge_Detector::imageCb, this);
     image_pub_ = it_.advertise("/edge_detector/raw_image", 1);
-    cv::namedWindow(OPENCV_WINDOW);
+    
+    //görsele nasıl abone oluyoruz ve yayınlıyor
+    //image.cv::namedWindow (), bir görüntüyü görüntülemek için, GUI oluşturan bir OpenCV işlevidir. 
+    //Bu işlevin içindeki argüman pencere adıdır. 
+    cv::namedWindow(OPENCV_WINDOW); 
 
   }
-
-  ~Edge_Detector()
+  ~Edge_Detector()//Sınıfı yıkıcı içinde, adlandırılmış pencereyi ile yok edilir.
   {
     cv::destroyWindow(OPENCV_WINDOW);
   }
 
+
+//Bu bir görüntü geri çağırma işlevidir ve temel olarak ROS görüntü mesajlarını CvBridge API’lerini kullanarak 
+//OpenCV cv::Mat türüne dönüştürür. ROS’u OpenCV’ye ve tersini nasıl dönüştürebileceğimiz aşağıdadır:
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
 
-    cv_bridge::CvImagePtr cv_ptr;
+    cv_bridge::CvImagePtr cv_ptr;//CvBridge ile başlamak için, bir CvImage örneği oluşturarak başlatılır
     namespace enc = sensor_msgs::image_encodings;
 
+/*ROS görüntü mesajını iki şekilde ele alabiliriz: ya bir kopya yapabiliriz görüntünün veya görüntü verilerini paylaşabiliriz. 
+ Resmi kopyaladığımızda, görüntüyü işler, ancak paylaşılan işaretçi kullanırsak, verileri değiştiremeyiz. 
+ ROS görüntüsünün bir kopyasını oluşturmak için toCvCopy() kullanılır, 
+ toCvShare() işlevi görüntünün işaretçisini elde etmek için kullanılır.*/ 
     try
     {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);//ROS görüntüsünün bir kopyasını oluşturur
     }
     catch (cv_bridge::Exception& e)
     {
@@ -49,6 +60,9 @@ public:
     }
 
     // Draw an example circle on the video stream
+    /*Görüntünün satırlarının ve sütunlarının belirli bir aralıkta olup olmadığını kontrol eder ve 
+    Eğer doğruysa, detect_edges (cv :: Mat) adında başka bir metodu çağırır ve parametre olarak verilen görüntüyü işleyin ve 
+    kenar algılanan görüntüyü görüntüleyin.*/
     if (cv_ptr->image.rows > 400 && cv_ptr->image.cols > 600){
 
 	detect_edges(cv_ptr->image);
